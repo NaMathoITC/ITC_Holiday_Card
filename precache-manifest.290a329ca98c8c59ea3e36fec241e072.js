7059faeeee50bd3ebc6d4e8e5f3cd0d8d54f60ee
chrisfal <chrisfal@users.noreply.github.com>
2020-04-06 18:14:47 -0700

    Updates

---
+++ precache-manifest.290a329ca98c8c59ea3e36fec241e072.js
@@ -0,0 +1,193 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ea757d5429b779d1b70de2a60cf3be",
+    "url": "/ITC_Holiday_Card/index.html"
+  },
+  {
+    "revision": "5b2c77712c789b46d222",
+    "url": "/ITC_Holiday_Card/static/css/2.2aac6f1d.chunk.css"
+  },
+  {
+    "revision": "5268b25886ba07bb0075",
+    "url": "/ITC_Holiday_Card/static/css/main.9c005ace.chunk.css"
+  },
+  {
+    "revision": "5b2c77712c789b46d222",
+    "url": "/ITC_Holiday_Card/static/js/2.5a1ac690.chunk.js"
+  },
+  {
+    "revision": "e65b7b851aa3e8b1269dc393cbbeb123",
+    "url": "/ITC_Holiday_Card/static/js/2.5a1ac690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5268b25886ba07bb0075",
+    "url": "/ITC_Holiday_Card/static/js/main.95e8632b.chunk.js"
+  },
+  {
+    "revision": "e7609cf6e79c27541eff",
+    "url": "/ITC_Holiday_Card/static/js/runtime-main.49ea1a3f.js"
+  },
+  {
+    "url": "/ITC_Holiday_Card/static/media/checkbcd1a436590360ed53ac2f46c76e43e4.bcd1a436.png"
+  },
+  {
+    "revision": "4a500d2728789b5d2a6255e4fc0d6959",
+    "url": "/ITC_Holiday_Card/static/media/chr1.4a500d27.png"
+  },
+  {
+    "revision": "fa203c8c8085820edf1300cb16ae5070",
+    "url": "/ITC_Holiday_Card/static/media/chr2.fa203c8c.png"
+  },
+  {
+    "revision": "f4d3c5bdcb52a2f5d533c2bf3800fa58",
+    "url": "/ITC_Holiday_Card/static/media/chr3.f4d3c5bd.png"
+  },
+  {
+    "revision": "8ae7a379bcd0d680266365afebc2ac7d",
+    "url": "/ITC_Holiday_Card/static/media/chr4.8ae7a379.png"
+  },
+  {
+    "revision": "544ce29a1cc96d695cf52f307d7d188c",
+    "url": "/ITC_Holiday_Card/static/media/chr5.544ce29a.png"
+  },
+  {
+    "revision": "2556d58c7388f1687fc4e96bfe1ea176",
+    "url": "/ITC_Holiday_Card/static/media/diw1.2556d58c.png"
+  },
+  {
+    "revision": "c7e5554ac66a61c43224f4ee19653a81",
+    "url": "/ITC_Holiday_Card/static/media/diw2.c7e5554a.png"
+  },
+  {
+    "revision": "53e0fe59804284d87b6eb28d0981047d",
+    "url": "/ITC_Holiday_Card/static/media/diw3.53e0fe59.png"
+  },
+  {
+    "revision": "76e214985f5b2601947f19cdaf9ab797",
+    "url": "/ITC_Holiday_Card/static/media/diw4.76e21498.png"
+  },
+  {
+    "revision": "1aa78b5309cbc6fad313f9b02fc84b57",
+    "url": "/ITC_Holiday_Card/static/media/diw5.1aa78b53.png"
+  },
+  {
+    "revision": "b773526e3eeb91f97b9408e22343a6c4",
+    "url": "/ITC_Holiday_Card/static/media/eas1.b773526e.png"
+  },
+  {
+    "revision": "dbeb2ff1bc984b1092f5ec70c3c9f9c7",
+    "url": "/ITC_Holiday_Card/static/media/eas2.dbeb2ff1.png"
+  },
+  {
+    "revision": "efd789dd2d8c8247c40948e9a9802dd4",
+    "url": "/ITC_Holiday_Card/static/media/eas3.efd789dd.png"
+  },
+  {
+    "revision": "48e4cb07faa6a5a6de0979a810725e49",
+    "url": "/ITC_Holiday_Card/static/media/eas4.48e4cb07.png"
+  },
+  {
+    "revision": "83ebacf8ad7a2307f39c2f2fe00e9a6b",
+    "url": "/ITC_Holiday_Card/static/media/eas5.83ebacf8.png"
+  },
+  {
+    "revision": "9a826f446f625bee095f8bf99e06b3fb",
+    "url": "/ITC_Holiday_Card/static/media/hal1.9a826f44.png"
+  },
+  {
+    "revision": "246d5d6c4563bed55804e61dfc62f2ff",
+    "url": "/ITC_Holiday_Card/static/media/hal2.246d5d6c.png"
+  },
+  {
+    "revision": "9dcc2bdd5c6815a4984135081a367cf9",
+    "url": "/ITC_Holiday_Card/static/media/hal3.9dcc2bdd.png"
+  },
+  {
+    "revision": "c5ab4c7fc9ced893f5dbe452aefe617a",
+    "url": "/ITC_Holiday_Card/static/media/hal4.c5ab4c7f.png"
+  },
+  {
+    "revision": "fcec774afdb30c56c68c2b24f2c39499",
+    "url": "/ITC_Holiday_Card/static/media/hal5.fcec774a.png"
+  },
+  {
+    "revision": "28bd1ea39f90207871f131f703876947",
+    "url": "/ITC_Holiday_Card/static/media/han1.28bd1ea3.png"
+  },
+  {
+    "revision": "36805a7aaabf07f58067a3c670385fda",
+    "url": "/ITC_Holiday_Card/static/media/han2.36805a7a.png"
+  },
+  {
+    "revision": "7dd02612ab599b6181d4e406d910b63d",
+    "url": "/ITC_Holiday_Card/static/media/han3.7dd02612.png"
+  },
+  {
+    "revision": "b9c698af5c5ceddf9932d942d9c7a94e",
+    "url": "/ITC_Holiday_Card/static/media/han4.b9c698af.png"
+  },
+  {
+    "revision": "45416877dbb38057462189e78d1f4a23",
+    "url": "/ITC_Holiday_Card/static/media/han5.45416877.png"
+  },
+  {
+    "revision": "73ffa6057a6757a4d0f8e7a6e44c0321",
+    "url": "/ITC_Holiday_Card/static/media/kwa1.73ffa605.png"
+  },
+  {
+    "revision": "99e38d62440f66339c6661357e47d46f",
+    "url": "/ITC_Holiday_Card/static/media/kwa2.99e38d62.png"
+  },
+  {
+    "revision": "fed11fc04445d245bf9713d3439e7198",
+    "url": "/ITC_Holiday_Card/static/media/kwa3.fed11fc0.png"
+  },
+  {
+    "revision": "49a9819b894965db1c9504fa2a131d9b",
+    "url": "/ITC_Holiday_Card/static/media/kwa4.49a9819b.png"
+  },
+  {
+    "revision": "6f91cebf4500ecf20d1728f4c2651549",
+    "url": "/ITC_Holiday_Card/static/media/kwa5.6f91cebf.png"
+  },
+  {
+    "revision": "ec9481e4dde3fcd39494a0f4f9e7c4e7",
+    "url": "/ITC_Holiday_Card/static/media/tha1.ec9481e4.png"
+  },
+  {
+    "revision": "76dbc96b3f6e7dd236c20cd8b74e5385",
+    "url": "/ITC_Holiday_Card/static/media/tha2.76dbc96b.png"
+  },
+  {
+    "revision": "fea1894f09118cff930afe4babeb7edb",
+    "url": "/ITC_Holiday_Card/static/media/tha3.fea1894f.png"
+  },
+  {
+    "revision": "919c8f506a281bfba8917822e251cad7",
+    "url": "/ITC_Holiday_Card/static/media/tha4.919c8f50.png"
+  },
+  {
+    "revision": "a6be21ca59174628c9ba22814a830f22",
+    "url": "/ITC_Holiday_Card/static/media/tha5.a6be21ca.png"
+  },
+  {
+    "revision": "7c6efc8eb3d3d350e7484b3035e8b925",
+    "url": "/ITC_Holiday_Card/static/media/val1.7c6efc8e.png"
+  },
+  {
+    "revision": "9b3f8e7986421ac195d07bca23d9f131",
+    "url": "/ITC_Holiday_Card/static/media/val2.9b3f8e79.png"
+  },
+  {
+    "revision": "20c1b70eb4a10326471d282e5b53ac6d",
+    "url": "/ITC_Holiday_Card/static/media/val3.20c1b70e.png"
+  },
+  {
+    "revision": "09e6ccda22f1cc6339526893ca32c48f",
+    "url": "/ITC_Holiday_Card/static/media/val4.09e6ccda.png"
+  },
+  {
+    "revision": "37fc74e2fae01bf56b7f3606850e2f8e",
+    "url": "/ITC_Holiday_Card/static/media/val5.37fc74e2.png"
+  }
+]);
